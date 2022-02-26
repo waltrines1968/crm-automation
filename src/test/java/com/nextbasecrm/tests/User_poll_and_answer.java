f6fbf74d2d8e6ef1e2a80709e987a8b88d18dcbb
SoftwareDev <devsoftware430@gmail.com>
2022-02-26 12:20:27 -0500

    user story is done

--- src/test/java/com/nextbasecrm/tests/User_poll_and_answer.java
+++ src/test/java/com/nextbasecrm/tests/User_poll_and_answer.java
@@ -3,7 +3,11 @@ package com.nextbasecrm.tests;
 import com.nextbasecrm.utilities.BrowserUtils;
 import com.nextbasecrm.utilities.ConfigurationReader;
 import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,6 +24,7 @@ public class User_poll_and_answer {
      * 4-click login button
      */
     WebDriver driver;
+
     @BeforeMethod
     public void setUp() {
         driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
@@ -36,12 +41,35 @@ public class User_poll_and_answer {
     }
 
     @Test
-    public void login_with_valid_credentials_with_login_btn(){
+    public void login_with_valid_credentials_with_login_btn() {
         // 2-write username
-
+        WebElement userName = driver.findElement(By.xpath("(//input[@class='login-inp'])[1]"));
+        userName.sendKeys(ConfigurationReader.getProperty("username"));
         //     * 3-write password
+        WebElement password = driver.findElement(By.xpath("(//input[@class='login-inp'])[2]"));
+        password.sendKeys(ConfigurationReader.getProperty("password"));
         //     * 4-click login button
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
+        BrowserUtils.sleep(3);
+        loginBtn.click();
+        // 5 verify title
+        String expectedTitle = "Portal";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
 
-
+    @Test
+    public void login_with_valid_credentials_with_enter_btn() {
+        // 2-write username
+        WebElement userName = driver.findElement(By.xpath("(//input[@class='login-inp'])[1]"));
+        userName.sendKeys(ConfigurationReader.getProperty("username"));
+        //     * 3-write password
+        WebElement password = driver.findElement(By.xpath("(//input[@class='login-inp'])[2]"));
+        password.sendKeys(ConfigurationReader.getProperty("password") + Keys.ENTER);
+        //     * 4-click login button
+        // 5 verify title
+        String expectedTitle = "Portal";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
 }
