595c880a12d02b78d059f05bd5b672a26fc66f54
SoftwareDev <devsoftware430@gmail.com>
2022-02-28 20:20:59 -0500

    Before method Alena US12

--- src/test/java/com/nextbasecrm/tests/US12_Announcement_Alena.java
+++ src/test/java/com/nextbasecrm/tests/US12_Announcement_Alena.java
@@ -1,5 +1,32 @@
 package com.nextbasecrm.tests;
 
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
 public class US12_Announcement_Alena {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //2. Go to https://login2.nextbasecrm.com/
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://login2.nextbasecrm.com/");
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
