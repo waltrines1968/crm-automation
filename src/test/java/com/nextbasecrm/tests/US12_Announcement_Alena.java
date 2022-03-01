8d93cff29c3bba740a281e9e893505c2b722b98c
SoftwareDev <devsoftware430@gmail.com>
2022-02-28 20:40:31 -0500

    Working on codes

--- src/test/java/com/nextbasecrm/tests/US12_Announcement_Alena.java
+++ src/test/java/com/nextbasecrm/tests/US12_Announcement_Alena.java
@@ -1,7 +1,9 @@
 package com.nextbasecrm.tests;
 
 import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
@@ -12,19 +14,28 @@ public class US12_Announcement_Alena {
 
     @BeforeMethod
     public void setupMethod(){
-        //2. Go to https://login2.nextbasecrm.com/
+        // Go to https://login2.nextbasecrm.com/
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://login2.nextbasecrm.com/");
     }
 
+        public void crm_login(){
+
+            WebElement inputUsername = driver.findElement(By.xpath("//input[@name = 'USER_LOGIN']"));
+            inputUsername.sendKeys("helpdesk79@cydeo.com");
+
+            WebElement inputPassword = driver.findElement(By.xpath("//input[@name = 'USER_PASSWORD']"));
+            inputPassword.sendKeys("UserUser");
 
 
+            WebElement loginButton = driver.findElement(By.xpath("//input[@value ='Log In']"));
+            loginButton.click();
+    }
 
-
-
-
+            //Find dropdown "More"
+            WebElement moreDropdown = driver.findElement(By.id("feed-add-post-form-link-text"));
 
 
 
