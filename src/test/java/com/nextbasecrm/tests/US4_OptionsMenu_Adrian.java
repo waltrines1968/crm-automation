126cbb0856af81c078d51a1268f76bd9ba3d949d
SoftwareDev <devsoftware430@gmail.com>
2022-03-02 00:37:30 -0500

    User story 4 completed.

--- src/test/java/com/nextbasecrm/tests/US4_OptionsMenu_Adrian.java
+++ src/test/java/com/nextbasecrm/tests/US4_OptionsMenu_Adrian.java
@@ -10,11 +10,8 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -44,17 +41,48 @@ public class US4_OptionsMenu_Adrian {
 
     @Test
     public void usernameBlock(){
+
+        //Click on Username block to open dropdown menu
         WebElement usernameBlock = driver.findElement(By.id("user-block"));
         usernameBlock.click();
 
+        //Check if 'My Profile' button is displayed
         WebElement myProfileButton = driver.findElement(By.xpath("//span[.='My Profile']"));
         System.out.println("myProfileButton.isDisplayed() = " + myProfileButton.isDisplayed());
 
         Assert.assertTrue(myProfileButton.isDisplayed(),"My Profile button is not displayed");
 
+        //Check if 'Edit Profile Settings' button is displayed
+        WebElement editProfileSettingsButton = driver.findElement(By.xpath("//span[.='Edit Profile Settings']"));
+        System.out.println("editProfileSettingsBlock = " + editProfileSettingsButton.isDisplayed());
+
+        Assert.assertTrue(editProfileSettingsButton.isDisplayed(),"Edit Profile Settings button is not displayed");
+
+        //Check if 'Themes' button is displayed
+        WebElement themesButton = driver.findElement(By.xpath("//*[@id=\"popup-window-content-menu-popup-user-menu\"]/div/div/span[1]/span[2]"));
+        System.out.println("themesButton = " + themesButton.isDisplayed());
+
+        Assert.assertTrue(themesButton.isDisplayed(), "Themes button is not displayed");
+
+        //Check if 'Configure Notifications' button is displayed
+        WebElement configureNotificationsButton = driver.findElement(By.xpath("//*[@id=\"popup-window-content-menu-popup-user-menu\"]/div/div/span[2]/span[2]"));
+        System.out.println("configureNotificationsButton = " + configureNotificationsButton.isDisplayed());
+
+        Assert.assertTrue(configureNotificationsButton.isDisplayed(), "Configure Notifications button is not displayed");
+
+        //Check if 'Log Out' button is displayed
+        WebElement logOutButton = driver.findElement(By.xpath("//span [.='Log out']"));
+        System.out.println("logOutButton = " + logOutButton.isDisplayed());
+
+        Assert.assertTrue(logOutButton.isDisplayed(), "Log out button is not displayed");
+
     }
 
-
+//Close all browsers after Test Method is run
+    @AfterMethod
+    public void exitBrowser(){
+        driver.quit();
+    }
 
 
 
