55819f97408710e9436fdc9124f0beb49fdf06fd
SoftwareDev <devsoftware430@gmail.com>
2022-03-02 13:18:51 -0500

    Working on codes

--- src/test/java/com/nextbasecrm/tests/US12_Announcement_Alena.java
+++ src/test/java/com/nextbasecrm/tests/US12_Announcement_Alena.java
@@ -4,13 +4,15 @@ import com.nextbasecrm.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class US12_Announcement_Alena {
 
-    public WebDriver driver;
+     WebDriver driver;
 
     @BeforeMethod
     public void setupMethod(){
@@ -20,8 +22,8 @@ public class US12_Announcement_Alena {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://login2.nextbasecrm.com/");
     }
-
-        public void crm_login(){
+        @Test
+        public void crm_login() {
 
             WebElement inputUsername = driver.findElement(By.xpath("//input[@name = 'USER_LOGIN']"));
             inputUsername.sendKeys("helpdesk79@cydeo.com");
@@ -32,12 +34,51 @@ public class US12_Announcement_Alena {
 
             WebElement loginButton = driver.findElement(By.xpath("//input[@value ='Log In']"));
             loginButton.click();
-    }
 
-            //Find dropdown "More"
+
             WebElement moreDropdown = driver.findElement(By.id("feed-add-post-form-link-text"));
+            moreDropdown.click();
+
+            WebElement announcement = driver.findElement(By.xpath("//span[.='Announcement']"));
+            announcement.click();
+
+            WebElement sendMessage = driver.findElement(By.xpath("//body[starts-with( 'true')]"));
+            sendMessage.sendKeys("Hello Alena");
 
 
 
 
-}
+
+
+
+        }
+
+
+
+
+            //Send message in announcement tab
+            public void iframeMessage(String hello_from_alena) {
+                WebElement sendMessage = driver.findElement(By.id("bx-html-editor-iframe-cnt-idPostFormLHE_blogPostForm"));
+                sendMessage.sendKeys("Hello Alena");
+
+
+                WebElement sendButton = driver.findElement(By.id("blog-submit-button-save"));
+
+                sendMessage.sendKeys("");
+                sendButton.click();
+                WebElement errorMessage = driver.findElement(By.xpath("//span[.='The message title is not specified']"));
+                String expectedResult = "The message title is not specified";
+                String actualResult = errorMessage.getText();
+
+                if (actualResult.equals(expectedResult)){
+                    System.out.println("Test is passed");
+                }else {
+                    System.out.println("Test is failed");
+                }
+
+
+            }
+
+
+        }
+
